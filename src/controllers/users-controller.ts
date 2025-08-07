2be2e7c7b62fd65e0ca182128f6253108ad0f357
Monique Dias <moniquedbandas@gmail.com>
2025-08-07 19:05:50 -0300

    projeto rocketlog

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { hash } from "bcrypt";
+import { z } from "zod";
+
+class UsersController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const hashedPassword = await hash(password, 8);
+    return response.json({ message: "ok", hashedPassword });
+  }
+}
+
+export { UsersController };
