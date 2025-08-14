c25d7aa472b7663a3665b537c28020267e3ec370
Monique Dias <moniquedbandas@gmail.com>
2025-08-14 16:35:55 -0300

    Parte do usuário feita.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
   async create(request: Request, response: Response) {
@@ -12,8 +14,24 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
     const hashedPassword = await hash(password, 8);
-    return response.json({ message: "ok", hashedPassword });
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
